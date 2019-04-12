975967e2f61dbc8b48e76dc7919c40e9ba86dd4d
cardumen <cardumen@cardumembpjuash.vtr.cl>
2019-04-12 16:36:47 -0400

    Advanced structuring and advanced propType (shape) added

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,25 +1,39 @@
 
 import React from 'react';
+
+import PropTypes from 'prop-types';
+
 import {WeatherExtraInfo} from './WeatherExtraInfo';
 import {WeatherTemperature} from './WeatherTemperature';
 
-
-
 //Uso y carga de multiples constantes en un archivo
-import {FOG} from './../../../constants/Weathers';
+// import {FOG} from './../../../constants/Weathers';
 
 import './weather-data.scss';
 
-export const WeatherData = ()=>{
+export const WeatherData = ({ finalData: {temperature, weatherState, humidity, wind} })=>{ //Destructuring con objetos con parámetros o más complejos
+
+    // const {temperature, weatherState, humidity, wind } = finalData;
 
     return(
         <div className="weather-data-cont">
             <span className="small-title">Weather Data</span>
             
-                <WeatherTemperature temperature={20} weatherstate={FOG}/>
+                <WeatherTemperature temperature={temperature} weatherstate={weatherState}/>
             
-            <div><WeatherExtraInfo humidity={80} wind={'10 m/s'}/></div>
+            <div><WeatherExtraInfo humidity={humidity} wind={wind}/></div>
         </div>
     )
 
 }
+
+
+//Esto es para validar estrictamente de que cada propiedad o prop de cada jsx sea explicitamente de tipo número, string, etc.
+WeatherData.propTypes = {
+    finalData: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+};
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,12 +4,23 @@ import React from 'react';
 import Location from './Location';
 import {WeatherData} from './WeatherData';
 
+import{FOG} from './../../constants/Weathers';
+
+const data = {
+    temperature: 15,
+    weatherState: FOG,
+    humidity: 10,
+    wind: '10 m/s',
+}
+
+
+
 const WeatherLocation = () =>{
 
     return(
         <div>
             <Location city={'chile'}/>
-            <WeatherData />
+            <WeatherData finalData={data}/>
         </div>
        
     );
