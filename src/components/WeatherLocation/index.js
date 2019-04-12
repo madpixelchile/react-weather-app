416aeb756d26c237078bc39dea280b9bd31c99dd
cardumen <cardumen@cardumembpjuash.vtr.cl>
2019-04-12 17:03:09 -0400

    Transformación de constante a clase y uso de react render

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import Location from './Location';
 import {WeatherData} from './WeatherData';
@@ -13,17 +13,17 @@ const data = {
     wind: '10 m/s',
 }
 
+class WeatherLocation extends Component {
 
-
-const WeatherLocation = () =>{
-
-    return(
-        <div>
-            <Location city={'chile'}/>
-            <WeatherData finalData={data}/>
-        </div>
-       
-    );
+    render(){
+        return(
+            <div>
+                <Location city={'chile'}/>
+                <WeatherData finalData={data}/>
+            </div>
+           
+        );
+    };
 
 }
 
