e11c76d45b862feec429e6b6a0c83a9604c8ab2b
cardumen <cardumen@cardumembpjuash.vtr.cl>
2019-05-02 12:34:36 -0400

    connect api rest

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -6,6 +6,15 @@ import {WeatherData} from './WeatherData';
 
 import{FOG} from './../../constants/Weathers';
 
+
+const location = "Santiago, CL";
+const api_key = "7332e4793f6b1ffdf08f63d552ddc17d";
+const url_base_weather = "https://samples.openweathermap.org/data/2.5/weather";
+
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
+
 const data = {
     temperature: 15,
     weatherState: FOG,
